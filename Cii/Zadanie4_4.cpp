9c270953a4bdbacbfdea744e82e560eb4fe7b204
Skaredin <sksv94@mail.ru>
2025-03-02 18:53:42 +0500

    4_4 calloc

--- Cii/Zadanie4_4.cpp
+++ Cii/Zadanie4_4.cpp
@@ -1,43 +1,41 @@
 #include <iostream>
 #include <cstring>  // Для strlen, strcmp, strcpy_s, strcat_s
 #include <locale>   // Для поддержки русского языка
-#include <cstdlib>  // Для malloc и free
+#include <cstdlib>  // Для malloc, calloc и free
 
 // Функция определения длины строки
-int dlina5(const char* str) {
+int dlina6(const char* str) {
     return strlen(str);
 }
 
 // Функция копирования строки 
-void kopir5(char* str1, const char* str2, size_t size) {
+void kopir6(char* str1, const char* str2, size_t size) {
     strcpy_s(str1, size, str2);
 }
 
 // Функция сравнения строк
-
-inline void sravn5(const char* str1, const char* str2) {
-        if (strcmp(str1, str2) == 0) {
-            std::cout << "Строки одинаковы" << std::endl;
-        }
-        else {
-            std::cout << "Строки разные" << std::endl;
-        }
+inline void sravn6(const char* str1, const char* str2) {
+    if (strcmp(str1, str2) == 0) {
+        std::cout << "Строки одинаковы" << std::endl;
     }
-
+    else {
+        std::cout << "Строки разные" << std::endl;
+    }
+}
 
 // Функция сцепления строк 
-void konkat5(char* str1, const char* str2, size_t size) {
+void konkat6(char* str1, const char* str2, size_t size) {
     strcat_s(str1, size, str2);
 }
 
-int main12() {
+int main() {
     setlocale(LC_ALL, "Russian"); // Устанавливаем локализацию для русского языка
 
     std::cout << "\033[32mСкаредин А.В. РИЗ-230916у\033[0m" << std::endl; // Зелёный цвет ФИО Группа
 
-    // Динамическое выделение памяти
-    char* str1 = (char*)malloc(100 * sizeof(char));
-    char* str2 = (char*)malloc(100 * sizeof(char));
+    // Динамическое выделение памяти с использованием calloc
+    char* str1 = (char*)calloc(100, sizeof(char));  // Используем calloc
+    char* str2 = (char*)calloc(100, sizeof(char));  // Используем calloc
 
     if (!str1 || !str2) {
         std::cerr << "Ошибка выделения памяти!" << std::endl;
@@ -45,34 +43,34 @@ int main12() {
     }
 
     // Инициализация строк
-    kopir5(str1, "qwerty", 100);
-    kopir5(str2, "1234567890", 100);
+    kopir6(str1, "qwerty", 100);
+    kopir6(str2, "1234567890", 100);
 
     // Вывод начальных значений строк
     std::cout << "Начальное значение str1 = \033[32m" << str1
         << "\033[0m, str2 = \033[32m" << str2 << "\033[0m" << std::endl;
 
-    std::cout << "Длина str1: \033[32m" << dlina5(str1) << "\033[0m" << std::endl;
-    std::cout << "Длина str2: \033[32m" << dlina5(str2) << "\033[0m" << std::endl;
+    std::cout << "Длина str1: \033[32m" << dlina6(str1) << "\033[0m" << std::endl;
+    std::cout << "Длина str2: \033[32m" << dlina6(str2) << "\033[0m" << std::endl;
 
-    sravn5(str1, str2);
+    sravn6(str1, str2);
 
     // Копирование str2 в str1
-    kopir5(str1, str2, 100);
+    kopir6(str1, str2, 100);
     std::cout << "Результат копирования: \033[32mstr1= " << str1
         << ", str2= " << str2 << "\033[0m" << std::endl;
 
-    sravn5(str1, str2);
+    sravn6(str1, str2);
 
     // Присваивание str1 заново "qwerty"
-    kopir5(str1, "qwerty", 100);
+    kopir6(str1, "qwerty", 100);
     std::cout << "Результат сравнения: \033[32mstr1= " << str1
         << ", str2= " << str2 << "\033[0m" << std::endl;
 
-    sravn5(str1, str2);
+    sravn6(str1, str2);
 
     // Сцепление строк
-    konkat5(str1, str2, 100);
+    konkat6(str1, str2, 100);
     std::cout << "Результат сцепления: \033[32mstr1= " << str1
         << ", str2= " << str2 << "\033[0m" << std::endl;
 
